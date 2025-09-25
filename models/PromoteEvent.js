ecb170ee6b3c09234807743dfc3d4c590c1f7f2b
2nd joy <juniorbat0003@gmail.com>
2025-09-25 13:25:04 +0600

    promote evet fixed

--- models/PromoteEvent.js
+++ models/PromoteEvent.js
@@ -10,6 +10,15 @@ const promoteEventSchema = new mongoose.Schema({
   contactEmail: { type: String, required: true },
   vipTicketQuantity: { type: Number, default: 0 },
   ticketQuantity: { type: Number, default: 0 },
-}, { timestamps: true,  collection: 'events'  });
+
+  // ✅ New fields
+  category: { 
+    type: String, 
+    required: true, 
+    enum: [ "Conferences", "Competitions", "Festivals", "Concerts", "Workshops", "Sports", "Theater", "Other"] 
+  },
+  price: { type: String, required: true } // can be "Free" or a number string
+}, 
+{ timestamps: true, collection: 'events' });
 
 module.exports = mongoose.model('PromoteEvent', promoteEventSchema);
